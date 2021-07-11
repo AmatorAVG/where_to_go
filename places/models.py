a256ecf151243f474bab48bfeaca2fb92359a2a6
AmatorAVG <amatoravg@gmail.com>
2021-07-11 09:24:59 +0700

    Add Place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+# class Image(models.Model):
+#     path = models.CharField(max_length=255)
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=255)
+    description_short = models.CharField(max_length=255)
+    description_long = models.TextField()
+    coordinates_lng = models.FloatField(default=0.00)
+    coordinates_lat = models.FloatField(default=0.00)
+    # imgs = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
