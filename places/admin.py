eaad1503a34b764f8aea7091b13b3eb95cf7f54b
AmatorAVG <amatoravg@gmail.com>
2021-08-03 11:00:32 +0700

    Add TabularInline for images

--- places/admin.py
+++ places/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import Place, Image
 
-admin.site.register(Place)
-admin.site.register(Image)
+# admin.site.register(Place)
+# admin.site.register(Image)
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    verbose_name_plural = 'Фотографии'
+    fieldsets = ((None, {'fields': ('img', 'position',)}),)
+    extra = 1
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
--- places/models.py
+++ places/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=255)
-    description_short = models.CharField(max_length=255)
-    description_long = models.TextField()
-    coordinates_lng = models.FloatField(default=0.00)
-    coordinates_lat = models.FloatField(default=0.00)
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    description_short = models.CharField(max_length=255, verbose_name='Краткое описание')
+    description_long = models.TextField(verbose_name='Полное описание')
+    coordinates_lng = models.FloatField(default=0.00, verbose_name='Долгота')
+    coordinates_lat = models.FloatField(default=0.00, verbose_name='Широта')
     # placeId = models.CharField(max_length=255, default='')
 
     def __str__(self):
@@ -14,9 +14,9 @@ class Place(models.Model):
 
 
 class Image(models.Model):
-    position = models.IntegerField()
-    img = models.ImageField(max_length=255)
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    position = models.IntegerField(verbose_name='Позиция')
+    img = models.ImageField(max_length=255, verbose_name='Картинка')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='Место')
 
     def __str__(self):
         return str(self.position) + " " + self.place.title
