d057f044574547e7c11a7fa374a50dddbd2e1f12
AmatorAVG <amatoravg@gmail.com>
2021-09-05 08:57:16 +0700

    replace CharField to TextField

--- places/models.py
+++ places/models.py
@@ -4,11 +4,11 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
-    description_short = models.CharField(max_length=255, verbose_name='Краткое описание')
+    description_short = models.TextField(verbose_name='Краткое описание')
     description_long = HTMLField(verbose_name='Полное описание')
     coordinates_lng = models.FloatField(default=0.00, verbose_name='Долгота')
     coordinates_lat = models.FloatField(default=0.00, verbose_name='Широта')
-    # placeId = models.CharField(max_length=255, default='')
+
 
     def __str__(self):
         return self.title
